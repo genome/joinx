dcd9ec465b37cf9bb107474d5c989f4a0eaea66e
Travis Abbott <tabbott@genome.wustl.edu>
2011-02-02 17:47:21 -0600

    added integration test for sort

---
+++ integration-test/sort/sort.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+
+use File::Basename qw/dirname/;
+use File::Temp;
+use Test::More tests => 4;
+
+my $dir = dirname($0);
+# TODO: get rid of hard coded version # here
+my $exe = "$dir/../../bin/joinx1.0 sort";
+my $data_dir = "$dir/data"; 
+my $input = join(" ", glob("$data_dir/input*.bed"));
+my $expected = "$data_dir/expected.bed";
+
+my $fh = new File::Temp;
+my $tmpfile = $fh->filename;
+
+my $cmd = "$exe $input -o $tmpfile";
+my $rv = system($cmd);
+is($rv&127, 0, "executed $cmd");
+my $diff = `diff $tmpfile $expected`;
+is($diff, '', "results are as expected (unstable sort)");
+
+# test stable sort too
+$cmd = "$exe -s $input -o $tmpfile";
+$rv = system($cmd);
+is($rv&127, 0, "executed $cmd");
+$diff = `diff $tmpfile $expected`;
+is($diff, '', "results are as expected (stable sort)");
+
+$fh->close();
+
+done_testing();
