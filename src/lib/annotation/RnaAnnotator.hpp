640f9b672189b6082c773245575b72d8644e341c
Travis Abbott <tabbott@genome.wustl.edu>
2011-02-03 14:52:30 -0600

    removed annotation, it is not being used.

--- src/lib/annotation/RnaAnnotator.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "TranscriptAnnotator.hpp"
-
-class RnaAnnotator : public TranscriptAnnotator {
-public:
-    std::string codingRegionString(const Variant& v, const TranscriptStructure& structure) const;
-
-};
