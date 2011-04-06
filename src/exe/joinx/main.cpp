f993ab38a87b6b14e83431278f3edbffba394750
Travis Abbott <tabbott@genome.wustl.edu>
2011-04-06 15:39:55 -0500

    better error message when cmdline args are specified multiple times
    boost program options was saying "multiple occurrences". now, we have
    a friendly error message giving the name of the offending option.

--- src/exe/joinx/main.cpp
+++ src/exe/joinx/main.cpp
@@ -1,14 +1,19 @@
 #include "JoinX.hpp"
 
+#include <boost/program_options.hpp>
 #include <iostream>
 #include <stdexcept>
 
 using namespace std;
+namespace po = boost::program_options;
 
 int main(int argc, char** argv) {
     try {
         JoinX app(argc, argv);
         app.exec();
+    } catch (const po::multiple_occurrences& e) {
+        cerr << "Error: multiple occurrences of option " << e.get_option_name()
+            << ". Abort." << endl;
     } catch (const exception& e) {
         cerr << e.what() << endl;
         return 1;
