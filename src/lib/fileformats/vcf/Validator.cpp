f12de27b02989df4b6ede71e219810f66a8dae0b
Rob Long <rlong@genome.wustl.edu>
2011-08-20 13:53:52 -0500

    Vcf Validator now validates info, filter, and format fields (though not per-sample format data)

--- src/lib/fileformats/vcf/Validator.cpp
+++ src/lib/fileformats/vcf/Validator.cpp
@@ -27,28 +27,68 @@ namespace {
 Validator::Validator(const Header& header)
     : _header(header)
     , _info(_header.category("INFO"))
-    , _filters(_header.category("FILTERS"))
+    , _filters(_header.category("FILTER"))
     , _format(_header.category("FORMAT"))
 {
     sort(_info.begin(), _info.end(), mapIdLessThan);
     sort(_filters.begin(), _filters.end(), mapIdLessThan);
     sort(_format.begin(), _format.end(), mapIdLessThan);
+ 
+    //populate _*_list's with fields defined in the header
+    for(auto it = _info.begin(); it != _info.end(); it++){
+        _info_list.insert((*it)["ID"]);
+    }
+    for(auto it = _filters.begin(); it != _filters.end(); it++){
+        _filters_list.insert((*it)["ID"]);
+    }
+    for(auto it = _format.begin(); it != _format.end(); it++){
+        _format_list.insert( (*it)["ID"] );
+    }
 }
 
 bool Validator::operator()(const Entry& e, std::vector<std::string>* problems) const {
     // TODO: loop over all the data in the entry that requires definitions in the header
     // Here is an example of how to check if the field "DP" exists in the _info section:
-    bool valid = true;
-    string value = "DP";
-    auto iter = lower_bound(_info.begin(), _info.end(), value, mapIdLessThanString);
-    if ((*iter)["ID"] != value) {
-        // BOOM
-    }
-    // TODO: could go even farther and validate the data is valid for (*iter)["Type"], etc
 
-    if (problems) {
-        problems->push_back("troll problem");
+    bool valid = true;
+
+    //check info fields
+    auto info_fields = e.info();
+    for(auto it=info_fields.begin();it!=info_fields.end();it++){
+        if(! _info_list.count((*it).id()) ) {
+            valid = false;
+            if (problems) {
+                problems->push_back( "Unknown field in info section: \'" + (*it).id() + "\'" );
+            }
+        }
     }
+
+    //check filter fields
+    auto filters_fields = e.failedFilters();
+    for(auto it=filters_fields.begin();it!=filters_fields.end();it++){
+        if(! _filters_list.count((*it)) ) {
+            valid = false;
+            if (problems) {
+                problems->push_back( "Unknown field in filter section: \'" + (*it) + "\'" );
+            }
+        }
+    }
+
+    //check format fields
+    auto format_fields = e.formatDescription();
+    for(auto it=format_fields.begin();it!=format_fields.end();it++){
+        Tokenizer<char>  t((*it), ':');
+        std::string field;
+        while( t.extract(field)){
+            if(! _format_list.count( field )) {
+                valid = false; 
+                if (problems) {
+                    problems->push_back( "Unknown field in format section: \'" + field + "\'" );
+                }
+            }
+        }
+    }
+
     return valid;
 }
 
