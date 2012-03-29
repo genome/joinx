8627c4a20c8a7605653e6ea0f1b9d54520188670
Dave Larson <dlarson@genome.wustl.edu>
2012-03-28 22:32:22 -0500

    writing R like C or Perl is bad. 1000X speedup on TsTv and Known callset metrics.

--- src/scripts/VcfReport.R
+++ src/scripts/VcfReport.R
@@ -61,55 +61,23 @@ per_site.maf_scatter=function(x) {
     print(ggplot(x, aes(x=c(1:nrow(x)),y=MAF)) + geom_point() + xlab("Minor Allele Frequency") + ylab("number") + opts(title = "Minor Allele Frequency Distribution"));
 }
 
-per_site.total_ts_tv=function(x) {
-    transition = 0;
-    transversion = 0;
-    wts = 0;
-    wtv = 0;
-    for(i in 1:nrow(x)) {
-        alts = unlist(strsplit(as.character(x[i,]$Alt),","));
-        transitionStatus = as.numeric(unlist(strsplit(as.character(x[i,]$ByAltTransition),",")));
-        sampleCount = as.numeric(unlist(strsplit(as.character(x[i,]$AlleleDistBySample),",")));
-        #alleleCount = as.numeric(unlist(strsplit(as.character(x[i,]$AlleleDist),",")));
-        for(a in 1:length(alts)) {
-            if(transitionStatus[a] && sampleCount[a+1]) {
-                transition = transition + 1;
-                wts = wts + sampleCount[a+1]; 
-            }
-            else {
-                if(!transitionStatus[a] && sampleCount[a+1]) {
-                    transversion = transversion + 1;
-                    wtv = wtv + sampleCount[a+1];
-                }
-            }
-        }
-    }
+per_site.total_ts_tv <- function(x) {
+    transitionStatus <- as.numeric(unlist(strsplit(as.character(x$ByAltTransition),",")))
+    sampleCount <- as.numeric(unlist(sapply(strsplit(as.character(x$AlleleDistBySample),","),"[",-1)))
+    transition = sum(transitionStatus[sampleCount>0])
+    transversion = sum(!transitionStatus[sampleCount>0])
+    wts = sum(sampleCount[transitionStatus>0])
+    wtv = sum(sampleCount[transitionStatus==0])
     list(transitions = transition, transversions = transversion, weightedTs = wts, weightedTv = wtv, unweighted_ts_tv_ratio = transition/transversion, weighted_ts_tv_ratio = wts/wtv);
 }
 
 per_site.total_known=function(x) {
-    novel = 0;
-    known = 0;
-    wnovel = 0;
-    wknown = 0;
-    for(i in 1:nrow(x)) {
-        alts = unlist(strsplit(as.character(x[i,]$Alt),","));
-        novelStatus = as.numeric(unlist(strsplit(as.character(x[i,]$ByAltNovel),",")));
-        sampleCount = as.numeric(unlist(strsplit(as.character(x[i,]$AlleleDistBySample),",")));
-        #alleleCount = as.numeric(unlist(strsplit(as.character(x[i,]$AlleleDist),",")));
-        for(a in 1:length(alts)) {
-            if(novelStatus[a] && sampleCount[a+1]) {
-                novel = novel + 1;
-                wnovel = wnovel + sampleCount[a+1]; 
-            }
-            else {
-                if(!novelStatus[a] && sampleCount[a+1]) {
-                    known = known + 1;
-                    wknown = wknown + sampleCount[a+1];
-                }
-            }
-        }
-    }
+    novelStatus = as.numeric(unlist(strsplit(as.character(x$ByAltNovel),",")));
+    sampleCount = as.numeric(unlist(sapply(strsplit(as.character(x$AlleleDistBySample),","),"[",-1)));
+    novel = sum(novelStatus[sampleCount>0]);
+    known = sum(!novelStatus[sampleCount>0]);
+    wnovel = sum(sampleCount[novelStatus>0]);
+    wknown = sum(sampleCount[novelStatus==0]);
     list(Novel = novel, Known = known, weightedNovel = wnovel, weightedKnown = wknown, unweighted_known_pct = known/(known+novel)*100, weighted_known_pct = wknown/(wknown+wnovel)*100);
 }
 
