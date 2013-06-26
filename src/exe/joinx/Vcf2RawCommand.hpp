b122b5d3eed49345109383e1a435a89f6c20d63a
Travis Abbott <tabbott@genome.wustl.edu>
2013-06-25 20:34:12 -0500

    add vcf2raw command to strip vcf entries down to raw alleles

---
+++ src/exe/joinx/Vcf2RawCommand.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "CommandBase.hpp"
+#include "fileformats/StreamHandler.hpp"
+
+#include <fstream>
+#include <string>
+#include <vector>
+
+class Vcf2RawCommand : public CommandBase {
+public:
+    using CommandBase::ptr;
+
+    Vcf2RawCommand();
+    ptr create(int argc, char** argv);
+
+    std::string name() const { return "vcf2raw"; }
+    std::string description() const {
+        return "convert vcf files to bed files";
+    }
+
+    void exec();
+
+protected:
+    void parseArguments(int argc, char** argv);
+
+protected:
+    std::string _vcfFile;
+    std::string _outFile;
+    std::string _refFa;
+    StreamHandler _streams;
+};
+
