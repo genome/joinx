433d8b0490725ed693979ccd57ec1b688f8eda5b
Travis Abbott <tabbott@genome.wustl.edu>
2013-10-31 12:45:48 -0500

    replace Region class with something useful

--- src/lib/common/Region.hpp
+++ src/lib/common/Region.hpp
@@ -2,88 +2,22 @@
 
 #include "common/cstdint.hpp"
 
-#include <sstream>
+#include <algorithm>
 
-class Region {
-public:
-    struct RelativePos {
-        enum Type {
-            ERROR,
-            BEFORE,
-            IN,
-            AFTER
-        };
+struct Region {
+    int64_t begin;
+    int64_t end;
 
-        RelativePos() : type(ERROR), dist(0) {}
-        RelativePos(Type t, int64_t d) : type(t), dist(d) {}
-        std::string toString() const {
-            std::stringstream rv;
-            switch (type) {
-                case BEFORE: rv << "-"; break;
-                case IN: break;
-                case AFTER:  rv << "*"; break;
 
-                case ERROR:
-                default: return ""; break;
-            }
-            rv << dist;
-            return rv.str();
-        }
+    Region()
+        : begin(0)
+        , end(0)
+    {}
 
-        bool in() const { return type == IN; }
-        bool before() const { return type == BEFORE; }
-        bool after() const { return type == AFTER; }
+    Region(int64_t begin, int64_t end)
+        : begin(begin)
+        , end(end)
+    {}
 
-        Type type;
-        int64_t dist;
-    };
-
-    Region();
-    Region(int strand, int64_t start, int64_t stop);
-
-    int strand() const;
-    int64_t start() const;
-    int64_t stop() const;
-    int64_t length() const;
-    int64_t strandedStart() const;
-    int64_t strandedStop() const;
-
-    RelativePos distance(int64_t pos) const;
-    int64_t distanceFromStart(int64_t pos) const;
-    int64_t distanceFromStop(int64_t pos) const;
-
-protected:
-    int _strand;
-    int64_t _start;
-    int64_t _stop;
-
-    int64_t _strandedStart;
-    int64_t _strandedStop;
-
-private:
-    bool (*strandedLess)(int64_t, int64_t);
+    int64_t overlap(Region const& that) const;
 };
-
-inline int Region::strand() const {
-    return _strand;
-}
-
-inline int64_t Region::start() const {
-    return _start;
-}
-
-inline int64_t Region::stop() const {
-    return _stop;
-}
-
-inline int64_t Region::length() const {
-    return stop()-start()+1;
-}
-
-inline int64_t Region::strandedStart() const {
-    return _strandedStart;
-}
-
-inline int64_t Region::strandedStop() const {
-    return _strandedStop;
-}
--- test/lib/common/TestRegion.cpp
+++ test/lib/common/TestRegion.cpp
@@ -1,87 +1,34 @@
 #include "common/Region.hpp"
 
-#include <string>
-#include <stdexcept>
 #include <gtest/gtest.h>
 
 using namespace std;
 
-TEST(TestRegion, strands) {
-    ASSERT_THROW(Region(0, 1, 2), runtime_error);
-    ASSERT_THROW(Region(2, 1, 2), runtime_error);
-    ASSERT_THROW(Region(-2, 1, 2), runtime_error);
-    Region fwd(1, 5, 10);
-    Region rev(-1, 5, 10);
-
-    ASSERT_EQ( 1, fwd.strand());
-    ASSERT_EQ( 5, fwd.start());
-    ASSERT_EQ(10, fwd.stop());
-    ASSERT_EQ( 5, fwd.strandedStart());
-    ASSERT_EQ(10, fwd.strandedStop());
-
-    ASSERT_EQ(-1, rev.strand());
-    ASSERT_EQ( 5, rev.start());
-    ASSERT_EQ(10, rev.stop());
-    ASSERT_EQ(10, rev.strandedStart());
-    ASSERT_EQ( 5, rev.strandedStop());
-
-    typedef Region::RelativePos RelPos;
-
-    RelPos pos = fwd.distance(4);
-    ASSERT_EQ(RelPos::BEFORE, pos.type);
-    ASSERT_EQ(1, pos.dist);
-    for (int i = 5; i <= 10; ++i) {
-        pos = fwd.distance(6);
-        ASSERT_EQ(RelPos::IN, pos.type);
-        ASSERT_EQ(0, pos.dist);
-    }
-    pos = fwd.distance(13);
-    ASSERT_EQ(RelPos::AFTER, pos.type);
-    ASSERT_EQ(3, pos.dist);
-
-    pos = rev.distance(4);
-    ASSERT_EQ(RelPos::AFTER, pos.type);
-    ASSERT_EQ(1, pos.dist);
-    for (int i = 5; i <= 10; ++i) {
-        pos = rev.distance(6);
-        ASSERT_EQ(RelPos::IN, pos.type);
-        ASSERT_EQ(0, pos.dist);
-    }
-    pos = rev.distance(13);
-    ASSERT_EQ(RelPos::BEFORE, pos.type);
-    ASSERT_EQ(3, pos.dist);
+TEST(TestRegion, overlapPartial) {
+    Region x(10, 20);
+    Region y(9, 12);
+    EXPECT_EQ(2, x.overlap(y));
+    EXPECT_EQ(2, y.overlap(x));
 }
 
-TEST(TestRegion, distance) {
-    Region fwd(1, 5, 10);
-    Region rev(-1, 5, 10);
-
-    ASSERT_EQ(0, fwd.distanceFromStart(5));
-    ASSERT_EQ(10, fwd.distanceFromStart(-5));
-    ASSERT_EQ(2, fwd.distanceFromStart(3));
-    ASSERT_EQ(1, fwd.distanceFromStart(4));
-    ASSERT_EQ(1, fwd.distanceFromStart(6));
-    ASSERT_EQ(2, fwd.distanceFromStart(7));
-    ASSERT_EQ(2, fwd.distanceFromStop(8));
-    ASSERT_EQ(1, fwd.distanceFromStop(9));
-    ASSERT_EQ(1, fwd.distanceFromStop(11));
-    ASSERT_EQ(2, fwd.distanceFromStop(12));
-
-    ASSERT_EQ(0, rev.distanceFromStart(10));
-    ASSERT_EQ(15, rev.distanceFromStart(-5));
-    ASSERT_EQ(2, rev.distanceFromStart(8));
-    ASSERT_EQ(1, rev.distanceFromStart(9));
-    ASSERT_EQ(1, rev.distanceFromStart(11));
-    ASSERT_EQ(2, rev.distanceFromStart(12));
-    ASSERT_EQ(2, rev.distanceFromStop(3));
-    ASSERT_EQ(1, rev.distanceFromStop(4));
-    ASSERT_EQ(1, rev.distanceFromStop(6));
-    ASSERT_EQ(2, rev.distanceFromStop(7));
+TEST(TestRegion, overlapContained) {
+    Region x(10, 20);
+    Region y(15, 18);
+    EXPECT_EQ(3, x.overlap(y));
+    EXPECT_EQ(3, y.overlap(x));
 }
 
-TEST(TestRegion, length) {
-    Region fwd(1, 20, 25);
-    Region rev(1, 20, 25);
-    ASSERT_EQ(6, fwd.length());
-    ASSERT_EQ(6, rev.length());
+TEST(TestRegion, overlapAdjacent) {
+    Region x(10, 20);
+    Region y(20, 30);
+    EXPECT_EQ(0, x.overlap(y));
+    EXPECT_EQ(0, y.overlap(x));
 }
+
+TEST(TestRegion, overlapDisjoint) {
+    Region x(10, 20);
+    Region y(90, 120);
+    EXPECT_EQ(0, x.overlap(y));
+    EXPECT_EQ(0, y.overlap(x));
+}
+
