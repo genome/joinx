65548b0ff190d3b684c1e5b8efbcfe8cfb078f6e
Travis Abbott <tabbott@genome.wustl.edu>
2013-11-01 11:46:28 -0500

    add integration test for vcf-filter

---
+++ integration-test/test_vcf_filter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from integrationtest import IntegrationTest, main
+import unittest
+
+class TestVcfFilter(IntegrationTest, unittest.TestCase):
+    def test_merge(self):
+        input_file = self.inputFiles("vcf-filter/input.vcf")[0]
+        output_file = self.tempFile("output.vcf")
+        for i in [2, 5, 6, 10]:
+            expected_file = self.inputFiles("vcf-filter/expected-d%d.vcf" % i)[0]
+
+            params = ["vcf-filter", input_file, "-d", str(i), "-o", output_file]
+            rv, err = self.execute(params)
+            if err:
+                print "STDERR:", err
+
+            self.assertEqual(0, rv)
+            self.assertEqual('', err)
+            self.assertFilesEqual(expected_file, output_file)
+
+if __name__ == "__main__":
+    main()
