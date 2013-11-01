31b78ad9a5797d6488c78ed335a510b886e73eb1
Travis Abbott <tabbott@genome.wustl.edu>
2013-11-01 15:20:52 -0500

    add integration test for vcf-report

---
+++ integration-test/test_vcf_report.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from integrationtest import IntegrationTest, main
+import unittest
+
+class TestVcfReport(IntegrationTest, unittest.TestCase):
+    def test_report(self):
+        input_file = self.inputFiles("vcf-report/input.vcf")[0]
+        expected_site_file = self.inputFiles("vcf-report/expected-site.txt")[0]
+        expected_sample_file = self.inputFiles("vcf-report/expected-sample.txt")[0]
+
+        output_site_file = self.tempFile("output-site.vcf")
+        output_sample_file = self.tempFile("output-sample.vcf")
+
+        params = ["vcf-report", "-i", input_file, "-s", output_site_file,
+                "-S", output_sample_file]
+        rv, err = self.execute(params)
+        if err:
+            print "STDERR:", err
+
+        self.assertEqual(0, rv)
+        self.assertEqual('', err)
+        self.assertFilesEqual(expected_site_file, output_site_file)
+        self.assertFilesEqual(expected_sample_file, output_sample_file)
+
+if __name__ == "__main__":
+    main()
