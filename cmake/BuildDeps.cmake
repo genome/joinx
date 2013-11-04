d7328ab4948a33c47e53176c70a0b7d5c8e61984
Travis Abbott <tabbott@genome.wustl.edu>
2013-11-04 17:35:19 -0600

    build our own zlib
    the version on ubuntu 10.04 dies when opening empty files

--- cmake/BuildDeps.cmake
+++ cmake/BuildDeps.cmake
@@ -34,3 +34,24 @@ function(build_boost BOOST_URL BUILD_DIR)
 
 endfunction(build_boost BOOST_VERSION BUILD_DIR)
 
+function(build_zlib ZLIB_URL BUILD_DIR)
+    set(ZLIB_ROOT ${CMAKE_BINARY_DIR}/vendor/zlib)
+    set(ZLIB_SRC ${CMAKE_BINARY_DIR}/vendor/zlib-src)
+    ExternalProject_Add(
+        zlib-1.2.8
+        URL ${ZLIB_URL}
+        SOURCE_DIR ${ZLIB_SRC}
+        BINARY_DIR ${ZLIB_SRC}
+        CONFIGURE_COMMAND ./configure --prefix=${ZLIB_ROOT}
+        BUILD_COMMAND make
+        INSTALL_COMMAND make install
+    )
+    set(ZLIB_INCLUDE_DIRS ${ZLIB_ROOT}/include PARENT_SCOPE)
+    set(ZLIB_LIBRARIES ${ZLIB_ROOT}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}z${CMAKE_STATIC_LIBRARY_SUFFIX}
+        PARENT_SCOPE)
+
+    add_library(z STATIC IMPORTED)
+    set_property(TARGET z PROPERTY IMPORTED_LOCATION ${ZLIB_LIBRARIES})
+
+    add_dependencies(deps zlib-1.2.8)
+endfunction(build_zlib ZLIB_URL BUILD_DIR)
