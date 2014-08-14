10c61548460f50f1dddfba6b08c1c0d79b5adc07
Travis Abbott <tabbott@genome.wustl.edu>
2014-08-14 15:17:57 -0500

    add reversed() helper for streamJoin()

--- src/lib/io/StreamJoin.hpp
+++ src/lib/io/StreamJoin.hpp
@@ -49,8 +49,33 @@ struct StreamJoin {
     char const* delim;
 };
 
+template<typename Iterable>
+struct Reversed {
+    typedef typename Iterable::const_reverse_iterator const_iterator_type;
+
+    Reversed(Iterable const& xs) : xs(xs) {}
+
+    const_iterator_type begin() const {
+        return xs.rbegin();
+    }
+
+    const_iterator_type end() const {
+        return xs.rend();
+    }
+
+    bool empty() const {
+        return xs.empty();
+    }
+
+    Iterable const& xs;
+};
 
 template<typename Iterable>
 StreamJoin<Iterable> streamJoin(Iterable const& seq) {
     return StreamJoin<Iterable>(seq);
+}
+
+template<typename Iterable>
+Reversed<Iterable> reversed(Iterable const& seq) {
+    return Reversed<Iterable>(seq);
 }
